e3d0003eb16f99dca652230d6d20d42a580a6630
Alex <forcablaugrana@gmail.com>
2023-06-12 22:41:59 +0300

    ghosts are made and moving

--- game.js
+++ game.js
@@ -14,10 +14,23 @@ let wallInnerColor = "black";
 let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 
+let foodColor = "#FEB897";
+
+let score = 0;
+let ghosts = [];
+let ghostCount = 4;
+
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
+
+let ghostLocations = [
+    {x: 0, y: 0},
+    {x: 176, y: 0},
+    {x: 0, y: 121},
+    {x: 176, y: 121}
+];
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -30,7 +43,7 @@ let map = [
     [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
@@ -45,24 +58,66 @@ let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+let randomTargetForGhosts = [
+    {x: 1 * oneBlockSize, y: 1 * oneBlockSize},
+    {x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize},
+    {x: (map[0].length - 2) * oneBlockSize, y: 1 * oneBlockSize},
+    {x: (map[0].length - 2) * oneBlockSize, y: (map.length - 2) * oneBlockSize}
+];
+
 let gameLoop = () => {
     update();
     draw();
 }
 
 let update = () => {
-    //todo 
+    
     pacman.moveProcess(); 
+    pacman.eat();  
+
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].moveProcess();
+    }
+}
+
+let drawGhosts = () => {
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].draw();
+    }
 }
 
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, "black");
-    
+    drawFood();
     drawWalls();
     pacman.draw();
+    drawScore();
+    drawGhosts();
+}
+
+let drawScore = () => {
+    canvasContext.font = "20px Emulogic";
+    canvasContext.fillStyle = "white";
+    canvasContext.fillText("Score: " + score, 0, oneBlockSize * (map.length + 1) + 10);
 }
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+
+
+let drawFood = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 2) {
+                createRect(j * oneBlockSize + oneBlockSize / 3,
+                i * oneBlockSize + oneBlockSize / 3,
+                oneBlockSize / 3,
+                oneBlockSize / 3,
+                foodColor);
+            }
+        }
+    }
+}
 
 let drawWalls = () => {
     for (let i = 0; i < map.length; i++) {
@@ -127,7 +182,27 @@ let createNewPackman = () => {
     )
 }
 
+let createGhosts = () => {
+    ghosts = [];
+    for (let i = 0; i < ghostCount; i++) {
+        let newGhost = new Ghost(
+            9 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+            10 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+            oneBlockSize,
+            oneBlockSize,
+            pacman.speed / 2,
+            ghostLocations[i % 4].x,
+            ghostLocations[i % 4].y,
+            124, 
+            116,
+            6 + i
+        );
+        ghosts.push(newGhost);
+    }
+}
+
 createNewPackman();
+createGhosts();
 gameLoop();
 
 window.addEventListener("keydown", (event) => {
--- pacman.js
+++ pacman.js
@@ -12,7 +12,7 @@ class Pacman {
 
         setInterval(() => {
             this.changeAnimation();
-        }, 100)
+        }, 100);
     }
 
     moveProcess() {
@@ -24,7 +24,16 @@ class Pacman {
     }
 
     eat() {
-
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[0].length; j++) {
+                if (map[i][j] == 2 && 
+                    this.getMapX() == j &&
+                    this.getMapY() == i) {
+                        map[i][j] = 3;
+                        score++;
+                    }
+            }
+        }
     }
 
     moveForward() {
